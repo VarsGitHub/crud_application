72da6af8323f245c7ab00eeba3f62e529e2d2f59
Vars <senyavasilenko@yandex.ru>
2021-07-30 17:08:37 +0300

    Backup commit

--- src/main/java/com/crud/vars/controller/UsersController.java
+++ src/main/java/com/crud/vars/controller/UsersController.java
@@ -4,7 +4,11 @@ import com.crud.vars.model.User;
 import com.crud.vars.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -49,5 +53,20 @@ public class UsersController {
     public String delete(@RequestParam(value = "id") int id) {
         userService.delete(id);
         return "redirect:/users";
+    }
+
+    @GetMapping(value = "/hello")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginPage() {
+        return "login";
     }
 }
